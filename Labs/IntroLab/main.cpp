1da1d433a419a4f001c817015b4503e29a3a1715
Garret Koontz <garretkoontz@Garrets-MacBook-Pro.local>
2019-06-18 12:02:45 -0700

    first commit

---
+++ Labs/IntroLab/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    string option;
+    
+    cin >> option;
+    
+    if (option == "blastoff")
+        cout << "Hello World" << endl;
+    exit(0);
+    return 0;
+}
+
